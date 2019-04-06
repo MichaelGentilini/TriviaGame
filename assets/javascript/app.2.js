6691cedd74eba141f53027e421af1764e94ff3db
Michael Gentilini <michael@dfwqqualityappraisal.com>
2019-04-06 08:37:59 -0500

    this is working.  The app.2.js is a bit DRYer

--- assets/javascript/app.2.js
+++ assets/javascript/app.2.js
@@ -3,73 +3,48 @@ $('document').ready(function () {
   // todo: create object for questions with answers and images
   var trivia = [{
       question: "What nationality was Cleopatra, Queen of Egypt?",
-      answer1: "Assyrian",
-      answer2: "Roman",
-      answer3: "Egyptian",
-      answer4: "Greek",
+      answer: ["Assyrian", "Roman", "Egyptian", "Greek"],
       correctAnswer: "Greek",
       image: "assets/images/greece.gif"
     },
     {
       question: "What color are aircraft black boxes?",
-      answer1: "dark yellow",
-      answer2: "bright orange",
-      answer3: "jet black",
-      answer4: "forest green",
+      answer: ["dark yellow", "bright orange", "jet black", "forest green"],
       correctAnswer: "bright orange",
       image: "assets/images/orange.gif"
     },
     {
       question: "In which U.S. state was Tennessee Williams born?",
-      answer1: "Alabama",
-      answer2: "Tennessee",
-      answer3: "Mississippi",
-      answer4: "North Carolina",
+      answer: ["Alabama", "Tennessee", "Mississippi", "North Carolina"],
       correctAnswer: "Mississippi",
       image: "assets/images/mississippi.gif"
     },
     {
       question: "What is the main ingredient of Bombay Duck?",
-      answer1: "Duck",
-      answer2: "Fish",
-      answer3: "Chicken",
-      answer4: "Pork",
+      answer: ["Duck", "Fish", "Chicken", "Pork"],
       correctAnswer: "Fish",
       image: "assets/images/fish.gif"
     },
     {
       question: "In which country was Caesar salad invented?",
-      answer1: "Mexico",
-      answer2: "Italy",
-      answer3: "Greece",
-      answer4: "Luxemburg",
+      answer: ["Mexico", "Italy", "Greece", "Luxemburg"],
       correctAnswer: "Mexico",
       image: "assets/images/mexico.gif"
     },
     {
       question: "After which animal are the Canary Islands named?",
-      answer1: "Dogs",
-      answer2: "Fish",
-      answer3: "Birds",
-      answer4: "Horses",
+      answer: ["Dogs", "Fish", "Birds", "Horses"],
       correctAnswer: "Dogs",
       image: "assets/images/dogs.gif"
     },
     {
       question: "Where did french fries originate?",
-      answer1: "United States",
-      answer2: "France",
-      answer3: "Belgium",
-      answer4: "Spain",
+      answer: ["United States", "France", "Belgium", "Spain"],
       correctAnswer: "Belgium",
       image: "assets/images/belgium.gif"
-    },
-    {
+    }, {
       question: "The average person does what thirteen times a day?",
-      answer1: "Stares",
-      answer2: "Swears",
-      answer3: "Smiles",
-      answer4: "Laughs",
+      answer: ["Stares", "Swears", "Smiles", "Laughs"],
       correctAnswer: "Laughs",
       image: "assets/images/laughter.gif"
     }
@@ -81,103 +56,183 @@ $('document').ready(function () {
   var unanswered = 0;
   var timer = 10;
   var questionCount = 0;
-  var isAnswered = false;
-
-  // var questionNumber = 0;
-  // isClicked = false;
 
   // *User is greeted with a "press to play" button to start the game with the tile showing at the top
 
-  var start = function () {
-    var welcome = $('<img id=runtime>');
-    welcome.attr("src", "https://cdn.dribbble.com/users/27231/screenshots/2432051/welcome.gif");
-    welcome.attr("width", "80%");
-    welcome.css("margin-top", "100px");
-    welcome.appendTo('#empty-question');
+  // ! here is the intro screen
 
-    var startBtn = $('<button>').addClass('btn btn-info');
-    startBtn.text('Press to start');
-    startBtn.appendTo('#empty-question');
-  }
-  // ! here is the start screen
+  var hello = function () {
+    var welcome = $("<img/>", {
+      id: "welcome-img",
+      src: "https://cdn.dribbble.com/users/27231/screenshots/2432051/welcome.gif",
+      alt: "welcome image"
+    });
+    welcome.appendTo("#empty-question");
+
+    var startBtn = $("<button>").addClass("btn btn-info");
+    startBtn.text("Press to start");
+    startBtn.appendTo("#empty-question");
+
+    $(".btn-info").click(function () {
+      $("#empty-question").empty();
+      $("#empty-question").append(question());
+    });
+  };
+
+  var question = function () {
+
+    // ! Countdown Control
+    var timerInterval;
+
+    function start() {
+      clearInterval(timerInterval);
+      timerInterval = setInterval(countDown, 1000);
+    }
+
+    function countDown() {
+      timer--;
+      $("#timer").html(timer);
+      if (timer <= 5) {
+        $("#timer").css("background-color", "yellow")
+        $("#timer").css("color", "red")
+        $("#timer").css("font-size", "40px")
+      }
+      if (timer === 0) {
+        stop();
+        unanswered++;
+        $("#empty-question").empty();
+        $("#empty-question").append(answered());
+        clearInterval(timerInterval);
+        timer = 10;
+      }
+    }
+
+    function stop() {
+      clearInterval(timerInterval);
+    }
 
 
-  // start();
-
-  // *After pushing the start button, a question with 4 possible answers populates the screen with a 10-20 second timer
-
-  var newQuestion = function () {
-
+    // *After pushing the start button, a question with 4 possible answers populates the screen with a timer
     //  * display timer
-    var displayTimer = $('<div>').addClass('timer');
-    displayTimer.text("You have " + timer + " seconds");
-    displayTimer.appendTo('#empty-question');
 
-    //  * display questions
-    for (let index = 1; index <= 4; index++) {
-      // var questionBox = $('<div>').addClass('questions');
-      var newDiv = $('<div>').addClass('questions');
-      var newBtn = $('<button>').addClass('btn btn-warning btn-lg').attr('value', trivia[questionCount].answer1);
-      // id = index;
-      newBtn.text("this is button " + index);
+    var displayTimer = $("<div>").addClass("timer");
+    displayTimer.html(
+      "You have " + "<span id=timer>" + timer + "</span>" + "  seconds "
+    );
+    displayTimer.appendTo("#empty-question");
 
-      // ?  how can I make .answer dynamically to match the index?
-      // newBtn.text("trivia" + [questionCount] + (".answer" + index));
+    // ! Userquestions go here
+    var newDiv = $('<div>').addClass('questions');
+
+    var userQuestion = $("<div>").addClass("userQuestion");
+    userQuestion.text(trivia[questionCount].question);
+    newDiv.append(userQuestion);
+
+    //! Dynamic Question (answer) BUTTONS
+    for (let i = 0; i < 4; i++) {
+      var newBtn = $('<button>');
+      newBtn.addClass('btn btn-warning btn-lg');
+      newBtn.text(trivia[questionCount].answer[i]);
       newDiv.append(newBtn);
       newDiv.appendTo('#empty-question');
     }
-  }
-  // ! here is the question screen
-  newQuestion();
+    start();
 
+    var answer = trivia[questionCount].correctAnswer;
+
+    //* add click function to button and reset timer
+    $(".btn-warning").click(function (event) {
+      var myAnswer = $(this).prop("text");
+      clearInterval(timerInterval);
+      timer = 10;
+      if (answer == myAnswer) {
+        right++;
+        $("#empty-question").empty();
+        $("#empty-question").append(answered());
+      } else {
+        wrong++;
+        $("#empty-question").empty();
+        $("#empty-question").append(answered());
+      }
+    });
+
+
+  };
+
+  // *Upon selecting an answer, the answer pops up with a gif/image .... If correct, right++, if wrong wrong++, if there is no answer, unsanswered is tracked
 
   var answered = function () {
+    var answerDiv = $("<div>").addClass("answers");
+    console.log(trivia[0].correctAnswer);
+    var correctAnswer = $("<div>").html(
+      "The answer is: " + trivia[questionCount].correctAnswer);
 
-    var answerDiv = $('<div>').addClass('answers');
-
-    var correctAnswer = $('<div>').text(
-      "The answer is: " + trivia[questionCount].correctAnswer
+    var correctImage = $("<div>").addClass("answer-img").html(
+      "<img src=" + trivia[questionCount].image + " width='50%'>"
     );
-    correctAnswer.css("padding-top", "50px");
-    correctAnswer.css("font-size", "40px");
-    correctAnswer.css("color", "red");
 
-    var correctImage = $('<div>').html("<img src=" + trivia[questionCount].image + " width='50%'>");
-    correctImage.css("margin-top", "50px");
-    // alert("Time Up!");
-
+    // * The answer is shown for a few seconds and then a new Question is provided
     answerDiv.append(correctAnswer, correctImage);
-    answerDiv.appendTo('#empty-question');
-  }
-  // ! here is the answer screen
-  // answered();
-
-
-  // todo if there is no answer, unsanswered is tracked
-
-  // if ((isAnswered == false) && (timer === 0)) {
-  //   unanswered++
-  // }
-
-  // *Upon selecting an answer, the answer pops up with a gif/image .... If correct, right++, if wrong wrong++
-
-  //? var userAnswer = data from click
-  //  todo if (userAnswer === Answer) {
-  //     right++
-  //   } else if (userAnswer == "") {
-  //     unanswered++
-  //   } else {
-  //     wrong++
-  //   }
+    answerDiv.appendTo("#empty-question");
+    setTimeout(newQuestion, 2000);
+  };
 
   // *Once a selection has been made, a new question is generated
-  // if ((isAnswered = true) || (timer === 0)) {
-  //   //  run newQuestion function to be defined
-  //   function newQuestion();
-  // }
+  function newQuestion() {
+    if (questionCount + 1 === trivia.length) {
+      questionCount++;
+      $("#empty-question").empty();
+      $("#empty-question").append(finalScore());
+    } else {
+      questionCount++;
+      $("#empty-question").empty();
+      $("#empty-question").append(question());
+    }
+  };
+
   // *Once all questions have been answered, the final score is shown
+  function finalScore() {
 
+    var displayComment = $('<h3>').addClass('comment');
 
+    if (right == trivia.length) {
+      displayComment.html("Wow, you're really smart!");
+    } else if (unanswered == trivia.length) {
+      displayComment.html("Is anybody home?");
+    } else {
+      displayComment.html("Good try!" + "<br>" + "You'll get all of them next time!");
+    }
 
+    var displayWins = $("<div>").addClass("wins");
+    displayWins.html("Correct: " + right);
 
+    var displayLosses = $("<div>").addClass("losses");
+    displayLosses.html("Incorrect : " + wrong);
+
+    var displayUnanswered = $("<div>").addClass("unanswered");
+    displayUnanswered.text("Unanswered: " + unanswered);
+
+    $("#empty-question").append(
+      displayComment,
+      displayWins,
+      displayLosses,
+      displayUnanswered
+    );
+    var endBtn = $("<button>").addClass("btn btn-info btn-end");
+    endBtn.text("want to play again?");
+    endBtn.appendTo("#empty-question");
+
+    $(".btn-end").click(function () {
+      right = 0;
+      wrong = 0;
+      unanswered = 0;
+      questionCount = 0;
+      $("#empty-question").empty();
+      $("#empty-question").append(hello());
+    });
+
+  }
+
+  // ! Start the game
+  hello();
 });
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,159 +1,296 @@
-// Set variables for questions with answers, track correct and incorrect answers, as well as the timer
-var trivia = [{
-    question: "What nationality was Cleopatra, Queen of Egypt?",
-    answerA: "Assyrian",
-    answerB: "Roman",
-    answerC: "Egyptian",
-    answerD: "Greek",
-    correctAnswer: "Greek",
-    image: "assets/images/greece.gif"
-  },
-  {
-    question: "What color are aircraft black boxes?",
-    answerA: "dark yellow",
-    answerB: "bright orange",
-    answerC: "jet black",
-    answerD: "forest green",
-    correctAnswer: "bright orange",
-    image: "assets/images/orange.gif"
-  },
-  {
-    question: "In which U.S. state was Tennessee Williams born?",
-    answerA: "Alabama",
-    answerB: "Tennessee",
-    answerC: "Mississippi",
-    answerD: "North Carolina",
-    correctAnswer: "Mississippi",
-    image: "assets/images/mississippi.gif"
-  },
-  {
-    question: "What is the main ingredient of Bombay Duck?",
-    answerA: "Duck",
-    answerB: "Fish",
-    answerC: "Chicken",
-    answerD: "Pork",
-    correctAnswer: "Fish",
-    image: "assets/images/fish.gif"
-  },
-  {
-    question: "In which country was Caesar salad invented?",
-    answerA: "Mexico",
-    answerB: "Italy",
-    answerC: "Greece",
-    answerD: "Luxemburg",
-    correctAnswer: "Mexico",
-    image: "assets/images/mexico.gif"
-  },
-  {
-    question: "After which animal are the Canary Islands named?",
-    answerA: "Dogs",
-    answerB: "Fish",
-    answerC: "Birds",
-    answerD: "Horses",
-    correctAnswer: "Dogs",
-    image: "assets/images/dogs.gif"
-  },
-  {
-    question: "Where did french fries originate?",
-    answerA: "United States",
-    answerB: "France",
-    answerC: "Belgium",
-    answerD: "Spain",
-    correctAnswer: "Belgium",
-    image: "assets/images/belgium.gif"
-  },
-  {
-    question: "The average person does what thirteen times a day?",
-    answerA: "Stares",
-    answerB: "Swears",
-    answerC: "Smiles",
-    answerD: "Laughs",
-    correctAnswer: "Laughs",
-    image: "assets/images/laughter.gif"
-  }
-];
-var right = 0;
-var wrong = 0;
-var unanswered = 0;
-var timer = 4;
-// var questionNumber = 0;
-// isClicked = false;
-
-// $(document).ready(function () {
-
-function reset() {
-  var timer = 11;
-}
-
-// alert("you got " + score + " out of " + trivia.length);
-// //  Variable that will hold our interval ID when we execute
-//  the "run" function
-var interval;
-
-//  When the stop button gets clicked, run the stop function.
-// $("#stop").on("click", stop);
-
-// //  When the resume button gets clicked, execute the run function.
-// $("#resume").on("click", run);
-
-//  The run function sets an interval
-//  that runs the decrement function once a second.
-//  *****BUG FIX********
-//  Clearing the interval prior to setting our new interval will not allow multiple instances.
-function run() {
-  clearInterval(interval);
-  interval = setInterval(decrement, 1000);
-}
-
-function question() {}
-//  The decrement function.
-function decrement() {
-  //  Decrease number by one.
-  timer--;
-
-  for (var j = 0; j < trivia.length; j++) {
-
-    console.log(trivia[j].question);
-    // show timer on screen
-    // $("#timer").html("<h3> You have " + timer + " seconds left</h3>");
-
-    // // display question and possible answers
-    // $("#empty-question").html("<h3>" + trivia[j].question + " </h3>");
-
-    // $("#answer-a").html("<button>" + trivia[j].answerA + "</button>");
-    // $("#answer-b").html("<button>" + trivia[j].answerB + "</button>");
-    // $("#answer-c").html("<button>" + trivia[j].answerC + "</button>");
-    // $("#answer-d").html("<button>" + trivia[j].answerD + "</button>");
-
-    // $("#answer-box > div > button").click(function () {
-    //   console.log('you answered: + trivia.answer');
-    //   // console.log(this.text);
-    // });
-
-    //  Once number hits zero...
-
-    if (timer === 0) {
-      //  ...run the stop function.
-      stop();
-      //  Alert the user that time is up.
-      $("#correct-answer").html(
-        "<h4>" + ("The answer is: " + trivia[j].correctAnswer) + " </h4>"
-      );
-      $("#empty-image").html("<img src=" + trivia[j].image + " width='30%'>");
-      // alert("Time Up!");
-      reset();
+$("document").ready(function () {
+  // todo: create object for questions with answers and images
+  var trivia = [{
+      question: "What nationality was Cleopatra, Queen of Egypt?",
+      answer1: "Assyrian",
+      answer2: "Roman",
+      answer3: "Egyptian",
+      answer4: "Greek",
+      correctAnswer: "Greek",
+      image: "assets/images/greece.gif"
+    },
+    {
+      question: "What color are aircraft black boxes?",
+      answer1: "dark yellow",
+      answer2: "bright orange",
+      answer3: "jet black",
+      answer4: "forest green",
+      correctAnswer: "bright orange",
+      image: "assets/images/orange.gif"
+    },
+    {
+      question: "What are Grape-Nuts made of?",
+      answer1: "corn and wheat",
+      answer2: "grape seeds",
+      answer3: "grapes and nuts",
+      answer4: "wheat and barley",
+      correctAnswer: "wheat and barley",
+      image: "assets/images/barley.gif"
+    },
+    {
+      question: "In what state do the New York Giants play their home games",
+      answer1: "New York",
+      answer2: "Massachusetts",
+      answer3: "New Jersey",
+      answer4: "Connecticut",
+      correctAnswer: "New Jersey",
+      image: "assets/images/jersey.gif"
+    },
+    {
+      question: "What is the place of origin of the Jerusalem artichoke?",
+      answer1: "North America",
+      answer2: "South America",
+      answer3: "Middle East",
+      answer4: "Asia",
+      correctAnswer: "North America",
+      image: "assets/images/north-america.gif"
+    },
+    {
+      question: "In which U.S. state was Tennessee Williams born?",
+      answer1: "Alabama",
+      answer2: "Tennessee",
+      answer3: "Mississippi",
+      answer4: "North Carolina",
+      correctAnswer: "Mississippi",
+      image: "assets/images/mississippi.gif"
+    },
+    {
+      question: "What is the main ingredient of Bombay Duck?",
+      answer1: "Duck",
+      answer2: "Fish",
+      answer3: "Chicken",
+      answer4: "Pork",
+      correctAnswer: "Fish",
+      image: "assets/images/fish.gif"
+    },
+    {
+      question: "In which country was Caesar salad invented?",
+      answer1: "Mexico",
+      answer2: "Italy",
+      answer3: "Greece",
+      answer4: "Luxemburg",
+      correctAnswer: "Mexico",
+      image: "assets/images/mexico.gif"
+    },
+    {
+      question: "After which animal are the Canary Islands named?",
+      answer1: "Dogs",
+      answer2: "Fish",
+      answer3: "Birds",
+      answer4: "Horses",
+      correctAnswer: "Dogs",
+      image: "assets/images/dogs.gif"
+    },
+    {
+      question: "Where did french fries originate?",
+      answer1: "United States",
+      answer2: "France",
+      answer3: "Belgium",
+      answer4: "Spain",
+      correctAnswer: "Belgium",
+      image: "assets/images/belgium.gif"
+    },
+    {
+      question: "The average person does what thirteen times a day?",
+      answer1: "Stares",
+      answer2: "Swears",
+      answer3: "Smiles",
+      answer4: "Laughs",
+      correctAnswer: "Laughs",
+      image: "assets/images/laughter.gif"
     }
+  ];
+
+  //todo: create variables to track answers and the timer
+
+  var right = 0;
+  var wrong = 0;
+  var unanswered = 0;
+  var timer = 10;
+  var questionCount = 0;
+
+  // *User is greeted with a "press to play" button to start the game with the tile showing at the top
+
+  // ! here is the intro screen
+
+  var intro = function () {
+    var welcome = $("<img/>", {
+      id: "welcome-img",
+      src: "https://cdn.dribbble.com/users/27231/screenshots/2432051/welcome.gif",
+      alt: "welcome image"
+    });
+    welcome.appendTo("#empty-question");
+
+    var startBtn = $("<button>").addClass("btn btn-info");
+    startBtn.text("Press to start");
+    startBtn.appendTo("#empty-question");
+
+    $(".btn-info").click(function () {
+      $("#empty-question").empty();
+      $("#empty-question").append(question());
+    });
+  };
+
+  var question = function () {
+
+    // ! Countdown Control
+    var timerInterval;
+
+    function start() {
+      clearInterval(timerInterval);
+      timerInterval = setInterval(countDown, 1000);
+    }
+
+    function countDown() {
+      timer--;
+      $("#timer").html(timer);
+      if (timer <= 5) {
+        $("#timer").css("background-color", "yellow")
+        $("#timer").css("color", "red")
+        $("#timer").css("font-size", "40px")
+      }
+      if (timer === 0) {
+        stop();
+        unanswered++;
+        $("#empty-question").empty();
+        $("#empty-question").append(answered());
+        clearInterval(timerInterval);
+        timer = 10;
+      }
+    }
+
+    function stop() {
+      clearInterval(timerInterval);
+    }
+
+    // *After pushing the start button, a question with 4 possible answers populates the screen with a timer
+
+    var displayTimer = $("<div>").addClass("timer");
+    displayTimer.html(
+      "You have " + "<span id=timer>" + timer + "</span>" + "  seconds "
+    );
+    displayTimer.appendTo("#empty-question");
+
+    // ! questions go here
+    var newDiv = $("<div>").addClass("questions");
+
+    var userQuestion = $("<div>").addClass("userQuestion");
+    userQuestion.text(trivia[questionCount].question);
+
+    var newBtn = $("<button>")
+      .addClass("btn btn-warning btn-lg")
+    newBtn.text(trivia[questionCount].answer1);
+
+    var newBtn2 = $("<button>")
+      .addClass("btn btn-warning btn-lg")
+    newBtn2.text(trivia[questionCount].answer2);
+
+    var newBtn3 = $("<button>")
+      .addClass("btn btn-warning btn-lg")
+    newBtn3.text(trivia[questionCount].answer3);
+
+    var newBtn4 = $("<button>")
+      .addClass("btn btn-warning btn-lg")
+    newBtn4.text(trivia[questionCount].answer4);
+
+    newDiv.append(userQuestion, newBtn, newBtn2, newBtn3, newBtn4);
+    newDiv.appendTo("#empty-question");
+
+    start();
+
+    var answer = trivia[questionCount].correctAnswer;
+
+    //* add click function to button and reset timer
+    $(".btn-warning").click(function (event) {
+      var myAnswer = $(this).prop("text");
+      clearInterval(timerInterval);
+      timer = 10;
+      if (answer == myAnswer) {
+        right++;
+        $("#empty-question").empty();
+        $("#empty-question").append(answered());
+      } else {
+        wrong++;
+        $("#empty-question").empty();
+        $("#empty-question").append(answered());
+      }
+    });
+  };
+
+  // *Upon selecting an answer, the answer pops up with a gif/image .... If correct, right++, if wrong wrong++, if there is no answer, unsanswered is tracked
+
+  var answered = function () {
+    var answerDiv = $("<div>").addClass("answers");
+
+    var correctAnswer = $("<div>").html(
+      "The answer is: " + trivia[questionCount].correctAnswer
+    );
+
+    var correctImage = $("<div>").addClass("answer-img").html(
+      "<img src=" + trivia[questionCount].image + " width='50%'>"
+    );
+
+    // * The answer is shown for a few seconds and then a new Question is provided
+    answerDiv.append(correctAnswer, correctImage);
+    answerDiv.appendTo("#empty-question");
+    setTimeout(newQuestion, 2000);
+  };
+
+  // *Once a selection has been made, a new question is generated
+  function newQuestion() {
+    if (questionCount + 1 === trivia.length) {
+      questionCount++;
+      $("#empty-question").empty();
+      $("#empty-question").append(finalScore());
+    } else {
+      questionCount++;
+      $("#empty-question").empty();
+      $("#empty-question").append(question());
+    }
+  };
+
+  // *Once all questions have been answered, the final score is shown
+  function finalScore() {
+
+    var displayComment = $('<h3>').addClass('comment');
+
+    if (right == trivia.length) {
+      displayComment.html("Wow, you're really smart!");
+    } else if (unanswered == trivia.length) {
+      displayComment.html("Is anybody home?");
+    } else {
+      displayComment.html("Good try!" + "<br>" + "You'll get all of them next time!");
+    }
+
+    var displayWins = $("<div>").addClass("wins");
+    displayWins.html("Correct: " + right);
+
+    var displayLosses = $("<div>").addClass("losses");
+    displayLosses.html("Incorrect : " + wrong);
+
+    var displayUnanswered = $("<div>").addClass("unanswered");
+    displayUnanswered.text("Unanswered: " + unanswered);
+
+    $("#empty-question").append(
+      displayComment,
+      displayWins,
+      displayLosses,
+      displayUnanswered
+    );
+    var endBtn = $("<button>").addClass("btn btn-info btn-end");
+    endBtn.text("want to play again?");
+    endBtn.appendTo("#empty-question");
+
+    $(".btn-end").click(function () {
+      right = 0;
+      wrong = 0;
+      unanswered = 0;
+      questionCount = 0;
+      $("#empty-question").empty();
+      $("#empty-question").append(intro());
+    });
   }
-}
 
-//  The stop function
-function stop() {
-  //  Clears our interval
-  //  We just pass the name of the interval
-  //  to the clearInterval function.
-  clearInterval(interval);
-}
-
-//  Execute the run function.
-run();
-// });
+  // ! Start the game
+  intro();
+});
